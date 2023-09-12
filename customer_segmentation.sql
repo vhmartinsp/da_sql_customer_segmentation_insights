543ee28da055f2c4b07a3097e431582316c84820
Victor Hugo <140098664+vhmartinsp@users.noreply.github.com>
2023-09-11 21:42:53 -0300

    customer_segmentation.sql

--- customer_segmentation.sql
+++ customer_segmentation.sql
@@ -1,27 +1,28 @@
 #%% Mostrar tabela
-SELECT * FROM customers_segmentation
+CREATE VIEW customer_segmentation AS
+SELECT * FROM customers_segmentation;
 
 #%% Dados Gerais
-SELECT AVG(age) AS average_age
-FROM customers_segmentation;
-
-#%% 
-SELECT AVG(annual_income) AS average_income
-FROM customers_segmentation;
-
-#%% 
-SELECT AVG(spending_score) AS spending_score_average
-FROM customers_segmentation;
-
-#%% 
+CREATE VIEW general_data AS
 SELECT
- CASE 
+  AVG(age) AS average_age,
+  AVG(annual_income) AS average_income,
+  AVG(spending_score) AS spending_score_average
+FROM customers_segmentation;
+
+
+#%% 
+CREATE VIEW gender_counts AS
+SELECT
+  CASE 
     WHEN gender = 'Female' THEN 'Mulher'
     WHEN gender = 'Male' THEN 'Homem'
     ELSE gender
-END AS genero_traduzido,
-    COUNT(*) AS total_customers,
-    ROUND((COUNT(*) / (SELECT COUNT(*) FROM customers)) * 100, 2) AS porcentagem_por_genero
+  END AS genero_traduzido,
+  COUNT(*) AS total_customers,
+  ROUND((COUNT(*) / (SELECT COUNT(*) FROM customers)) * 100, 2) AS porcentagem_por_genero
+FROM customers_segmentation
+GROUP BY gender;
  
 FROM
     customers_segmentation
@@ -29,29 +30,26 @@ GROUP BY
     gender;
 
 #%% 
+CREATE VIEW vw_profession_counts AS
 SELECT
-    profession,
-    COUNT(customer_id) AS total_customer
-FROM
-    customers_segmentation
-GROUP BY
-    profession
-ORDER BY
-    2 DESC;
+  profession,
+  COUNT(customer_id) AS total_customer
+FROM customers_segmentation
+GROUP BY profession
+ORDER BY total_customer DESC;
 
 #%% Médias de score de gasto por profissão
+CREATE VIEW avg_scores_by_profession AS
 SELECT
-    profession AS profissao,
-    ROUND(AVG(spending_score), 2) AS avg_scores_gastos,
-    COUNT(profession) AS profissao_clientes_total
-FROM
-    customers_segmentation
-GROUP BY
-    profession
-ORDER BY
-    profissao_clientes_total DESC;
+  profession AS profissao,
+  ROUND(AVG(spending_score), 2) AS avg_scores_gastos,
+  COUNT(profession) AS profissao_clientes_total
+FROM customers_segmentation
+GROUP BY profession
+ORDER BY profissao_clientes_total DESC;
  
 #%% Relação entre poder de consumo e gênero
+CREATE VIEW gender_spending_score AS
 SELECT
   COUNT(gender) AS quantidade_pessoas_por_genero,
   spending_score AS score_gastos,
